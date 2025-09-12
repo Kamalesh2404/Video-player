cf1d412e2c7ba4c4089c17a4e5e3130813089b69
2441213 <2441213@cognizant.com>
2025-09-12 10:01:07 +0530

    create:Navbar

--- PatientWellnessMonitoring/src/app/app.routes.ts
+++ PatientWellnessMonitoring/src/app/app.routes.ts
@@ -3,7 +3,45 @@ import { Login } from './Components/login/login';
 import { Signup } from './Components/signup/signup';
 
 export const routes: Routes = [
-  { path: 'login', component: Login },
-  { path: 'signup', component: Signup },
-  { path: '**', redirectTo: 'login', pathMatch: 'full' },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'signup',
+        component: Signup,
+    },
+    // {
+    //     path: 'doctor',
+    //     // canActivate: [authGuard, doctorGuard],
+    //     component: DoctorDashboard
+    // },
+    // {
+    //     path: 'doctor/assign-plans',
+    //     component: WellnessPlan
+    // },
+    // {
+    //     path: 'doctor/create-plan',
+    //     canActivate: [authGuard, doctorGuard],
+    //     component: CreatePlanPageComponent
+    // },
+    // {
+    //     path: 'doctor/patient-progress/:id',
+    //     canActivate: [authGuard, doctorGuard],
+    //     component: PatientProgressPageComponent
+    // },
+    // {
+    //     path: 'patient',
+    //     component: PatientDashboard
+    // },
+    // {
+    //     path: '',
+    //     redirectTo: 'login',
+    //     pathMatch: 'full'
+    // },
+    // {
+    //     path: '**',
+    //     redirectTo: 'login' // Redirect to login for any unknown paths
+    // }
 ];
+
